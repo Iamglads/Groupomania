63b778cff2543278be9496ad65bfb918161367f4
Gladston Aristoverne <gladston.aristoverne.ga@gmail.com>
2020-09-23 13:29:48 -0400

    routing ok

--- groupomania-frontend/src/router/index.js
+++ groupomania-frontend/src/router/index.js
@@ -1,19 +1,35 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Signup from '../views/Signup.vue'
+import Marketplace from '../views/Marketplace.vue'
+import Profil from '../views/Profil.vue'
+import Login from '../views/Login.vue'
 
 const routes = [
   {
     path: '/',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: Signup
+  },
+  {
+    path: '/home',
     name: 'Home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/profil',
+    name: 'Profil',
+    component: Profil
+  },
+  {
+    path: '/marketplace',
+    name: 'Marketplace',
+    component: Marketplace
   }
 ]
 
