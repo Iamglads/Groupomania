5934058bab6dafd1917c33d85999f79fd45ae6c1
Gladston Aristoverne <gladston.aristoverne.ga@gmail.com>
2020-09-23 15:57:28 -0400

    require bootstrap css

--- groupomania-frontend/src/main.js
+++ groupomania-frontend/src/main.js
@@ -2,4 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+require('../node_modules/bootstrap/dist/css/bootstrap.css')
+
+require('../node_modules/bootstrap/dist/css/bootstrap.css')
+
 createApp(App).use(router).mount('#app')
