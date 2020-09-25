9004a1eb8f5a7126bdf3dddfa72fba3745686407
Gladston Aristoverne <gladston.aristoverne.ga@gmail.com>
2020-09-24 20:44:52 -0400

    delete bootstrap cdn

--- groupomania-frontend/src/main.js
+++ groupomania-frontend/src/main.js
@@ -2,8 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-require('../node_modules/bootstrap/dist/css/bootstrap.css')
 
-require('../node_modules/bootstrap/dist/css/bootstrap.css')
 
 createApp(App).use(router).mount('#app')
