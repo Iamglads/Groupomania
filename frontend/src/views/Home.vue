9f7c6a7917d345f323a497d96c7d48359b142b96
Gladston Aristoverne <gladston.aristoverne.ga@gmail.com>
2020-10-18 18:15:04 -0400

    delete post"

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="home">
         <Sidebar />
-        <div class="home-content row col-md-6 ">
-            <div class="home-content-block ">
+        <div class="home__content row col-md-6 ">
+            <div class="home__content--block ">
                 <h1> Publication </h1>
                 <div class="divider"></div>
                 <!-- post form -->
-                <form>
+                <form class="home__post--form">
                     <div class="form-group">
                         <label></label>
                         <input 
@@ -27,12 +27,13 @@
                         />
                     </div>
                     <div class="form-group">
-                        <label for="exampleFormControlFile1">Ajouter une image</label>
+                        <label for="exampleFormControlFile1"></label>
                         <input 
-                        @change="previewFiles"
+                        @change="selectFile()"
+                        accept="images/*"
                         type="file" 
                         class="form-control-file" 
-                        ref="myFiles"
+                        ref="file"
                         id="file" 
                         />
                     </div>
@@ -44,9 +45,9 @@
                 <!-- form add post -->
                  <hr class="my-4">
                 <!-- all posts -->
-                <section  class="home-content-info">
+                <section  class="home-post">
                     <!-- boucle dans le tableau afin de recupérer tous les posts -->
-                    <article v-for="currentPost in currentPosts" :key="currentPost.id"  class="home-content-posts">
+                    <article v-for="currentPost in currentPosts" :key="currentPost.id"  class="post-bloc">
                         <p> 
                             <i class="fas fa-user"></i> 
                              {{currentPost.User.firstname}}
@@ -55,15 +56,17 @@
                         <p>{{ moment(currentPost.createdAt).fromNow() }}</p>
                          <hr class="my-1">
                         <h3>  {{ currentPost.title}} </h3>
-                        <div class="home-content-posts-img">
+                        <!-- image publication -->
+                        <div class="post-img">
                             <img  src="../assets/glad.jpg" alt="photo">
                         </div>
-                        <div class="home-content-text">
+                        <!-- text publication -->
+                        <div class="post-content">
                             <p>  {{ currentPost.content}}  </p>
                         </div>
-                        <hr class="my-4">
-                        <!-- likes and comments -->
-                        <div class="actions-post">
+                          <!-- likes and comments -->
+                          <hr class="my-1">
+                        <div class="post-actions">
                             <div>
                                 <i class="fas fa-thumbs-up"></i>
                                 <span> 0 </span>
@@ -72,15 +75,28 @@
                                 <i class="fas fa-comment-dots"></i> 
                                 <span> 0 </span>
                             </div> 
-                            <!-- Upadate post -->
-                             <div v-on:click="showForm" class="cursor-pointer">
-                                <i class="fas fa-user-edit"></i>
-                            </div>
                             <!-- Delete post-->
-                             <div @click="deletePost()" class="cursor-pointer">
-                                <i class="far fa-trash-alt"></i>
+                             <div
+                             v-if="currentUser.id == currentPost.UserId"
+                              @click.prevent="deletePost(currentPost.id)" 
+                              class="cursor-pointer">
+                                <i  class="far fa-trash-alt"></i>
                             </div>
                         </div>
+                        <hr class="my-1">
+                        <!-- commentaire publication -->
+                        <div class="post-commment">
+                             <input 
+                            type="text" 
+                            class="form-control" 
+                            placeholder="Commentaire" 
+                            />
+                        </div> 
+                        <!-- display comments -->
+                        <div class="post-commment">
+                          <p>Les commentaires seront affichés ici</p>
+                        </div> 
+                        
                     </article>
                 </section>
                 <!-- all posts end-->
@@ -94,6 +110,7 @@
 import Sidebar from '../components/Sidebar'
 import axios from 'axios'
 const moment = require('moment')
+moment.locale('fr')
 
 
 export default {
@@ -113,6 +130,12 @@ export default {
             }
         }
     },
+
+    computed: {
+      currentUser() {
+        return this.$store.state.auth.user
+    }
+    },
     // get and display all posts
     beforeCreate() {
             let userData = JSON.parse(localStorage.getItem("user"))
@@ -124,7 +147,7 @@ export default {
             })
             .then((response) => {
             this.currentPosts = response.data;
-            console.log(response.data);
+            //console.log(response.data);
             })
             .catch((err) => {console.log(err);});
         
@@ -135,7 +158,7 @@ export default {
             let data = {
                 title: this.post.title,
                 content: this.post.content,
-                attachment: this.previewFiles(),
+                attachment: this.selectFile(),
             }
 
             axios.post('http://localhost:3000/api/post', data, {
@@ -145,7 +168,7 @@ export default {
               },
             })
             .then((response) => {
-                this.post.userId = response.data.userId
+                this.post.id = response.data.id
                 console.log(response.data)
                 this.submitted = true
             })
@@ -153,33 +176,29 @@ export default {
             window.location.reload();  
             
         },
-        newPost() {
-            this.submitted = false
-            this.post = {}
-        },
 
-        previewFiles() {
-            this.files = this.$refs.myFiles.files
+        selectFile() {
+            this.post.attachment = this.$refs.file.files
         },
 
 
-        deletePost() {
+        deletePost(currentPostId) {
             let userData = JSON.parse(localStorage.getItem('user'))
             let token = userData.token
             let user = userData
             console.log(user)
 
-            axios.delete(`http://localhost:3000/api/post/${user.userId}`, {
+            axios.delete(`http://localhost:3000/api/post/${currentPostId}`, {
                 headers: {
                 Authorization: `Bearer ${token}`,
                 "Content-type": "application/json"
             },
             })
             .then(() => {
-                console.log('Vous avez supprimé votre compte!')
+                console.log('Vous avez supprimé votre publication!')
+                window.location.reload();
             })
             .catch(error  => { return error });
-            window.location.reload();     
             
         }       
 }
@@ -190,62 +209,58 @@ export default {
     .home{
         display: flex;
         justify-content: center;
-        h1{
-            text-align: left;
-        }
-    }
-
-    .home-content{
-        width: 80%;
-        display: flex;
-        justify-content: center;
-        margin-top: 120px;
-    }
-
-    .home-content-info{
-        .home-content-posts{
-            height: auto;
-            background: #ffffff;
-            border-radius: 20px;
-            padding: 1em;
-            margin-bottom: 10px;
-            .home-content-posts-img{
-                height: auto;
-                background:  rgb(167, 184, 208);
-                img{
-                    max-width: 100%;
+        .home__content{
+            width: 80%;
+            display: flex;
+            justify-content: center;
+            margin-top: 120px;
+            .home__post--form{
+                  .btn-post{
+                    padding: .3em;
+                    border: 2px solid #cf525c;
+                    color: #0a2042;
+                    background: none;
+                    margin-top: 30px;
+                    border-radius: 5px;
+                    &:hover{
+                        background:rgb(207, 82, 92);
+                        color: #ffffff;
+                    }
+                }
+            }
+            .home-post{
+                .post-bloc{
                     height: auto;
+                    background: #ffffff;
+                    border-radius: 20px;
+                    padding: 1em;
+                    margin-bottom: 10px;
+                    .post-img{
+                        height: auto;
+                        background:  rgb(167, 184, 208);
+                        img{
+                            max-width: 100%;
+                            height: auto;
+                        }
+                    }
+                    .post-actions{
+                        display: flex;
+                        justify-content: space-evenly;
+                        .fas{
+                            font-size: 20px;
+                        }
+                    }
+                    h3{
+                        text-align: left;
+                    }
+                    p{
+                        text-align: left;
+                    }
                 }
-            }
-             h3{
-                    text-align: left;
-                }
-            p{
-                text-align: left;
+
             }
         }
 
     }
-
-    .actions-post{
-        display: flex;
-        justify-content: space-evenly;
-        .fas{
-            font-size: 20px;
-        }
-    }
-
-       .btn-post{
-            padding: .3em;
-            border: 2px solid #cf525c;
-            color: #0a2042;
-            background: none;
-            margin-top: 30px;
-            border-radius: 5px;
-            &:hover{
-                background:rgb(207, 82, 92);
-                color: #ffffff;
-            }
-        }
 
 </style>
