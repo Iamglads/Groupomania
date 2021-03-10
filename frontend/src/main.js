ca0925ea1680955f28a36744044438cbe61aece5
Gladston Aristoverne <gladston.aristoverne.ga@gmail.com>
2021-03-10 17:37:21 -0300

    import de vue-material

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,12 +7,17 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import VeeValidate from 'vee-validate'
 
 
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
 require ('./assets/style.css')
 
 
 Vue.config.productionTip = false
 
 Vue.use(VeeValidate)
+Vue.use(VueMaterial)
 
 
 new Vue({
