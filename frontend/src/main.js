bfb128a2a70ed342ee301733d5dc355365a427a7
Gladston Aristoverne <gladston.aristoverne.ga@gmail.com>
2022-02-11 17:54:33 -0500

    test

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,34 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import VeeValidate from 'vee-validate'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import VeeValidate from "vee-validate";
 
+import "vue-material/dist/vue-material.min.css";
+import "vue-material/dist/theme/default.css";
+import vuetify from "./plugins/vuetify";
 
-<<<<<<< HEAD
+require("./assets/style.css");
 
+Vue.config.productionTip = false;
 
-
-import VueMaterial from 'vue-material'
-import 'vue-material/dist/vue-material.min.css'
-import 'vue-material/dist/theme/default.css'
-import vuetify from './plugins/vuetify';
-
-=======
->>>>>>> parent of ca0925e... import de vue-material
-require ('./assets/style.css')
-
-
-Vue.config.productionTip = false
-
-Vue.use(VeeValidate)
-
+Vue.use(VeeValidate);
 
 new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
